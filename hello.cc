d8b5de19aca503da76798a3c6a9f4527a6c559d2
Lackadaisica1 <39544752+Lackadaisica1@users.noreply.github.com>
2020-06-18 14:34:30 -0700

    hey i'm learning c++ now

---
+++ hello.cc
@@ -0,0 +1,11 @@
+// This program outputs the message ''Hello, World!'' to the monitor
+
+#include <iostream>
+using namespace std;
+
+int main() //C++ programs start by executing the funciton main
+{
+  cout<<"Hello, World!\n";
+  return 0;
+
+}
