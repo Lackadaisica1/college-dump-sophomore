e9716bcc181c1568cf56d6789f5ec0f46b766abd
Lackadaisica1 <39544752+Lackadaisica1@users.noreply.github.com>
2020-06-20 12:02:58 -0700

    Add files via upload

---
+++ helloimproved.cc
@@ -0,0 +1,12 @@
+// This program outputs the modified message of "Hello, programming, here we go!" to the terminal screen. Because I need to learn thing.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello, programming !\n Here we go!";
+  return 0;
+
+} 
+     
