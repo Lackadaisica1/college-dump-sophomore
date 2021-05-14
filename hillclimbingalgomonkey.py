bb7800d561893e3089c65a4e1c38303b267a6dd4
Lackadaisica1 <39544752+Lackadaisica1@users.noreply.github.com>
2021-05-13 23:35:29 -0700

    Add files via upload

--- hillclimbingalgomonkey.py
+++ hillclimbingalgomonkey.py
@@ -5,44 +5,46 @@
 # 3) Match this with the phrase "methinks it is a weasel", if not equal
 # then repeat step 2
 # 4) End the time, print difference
+# NOTE: THIS ONE IS EDITED FOR A "HILL CLIMBING ALGORITHM"
 
 import time
 import random
 
 
-# Function which generates random string 
+# Function which generates string randomly, and then returns False when done
+# Also, uses "hill climbing" algorithim
 def generate_string():
     list_of_roman_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", \
                               "i", "j", "k", "l", "m", "n", "o", "p", \
                               "q", "r", "s", "t", "u", "v", "w", "x", \
                               "y", "z", " "]
+
     
+    true_string = "methinks it is a weasel"
     test_string = ""
-    for i in range(len("methinks it is a weasel")):
+
+    # initializing an index for the key string
+    j = 0
+    
+    for i in range(len(true_string)):
         char = random.choice(list_of_roman_alphabet)
+        while char != true_string[j]:
+            char = random.choice(list_of_roman_alphabet)
         test_string += char
-    return test_string
+        j += 1
+    
+    return test_string    
+        
 
-
-def check_string(string):
-    if string == "methinks it is a weasel":
-        return False
-    else:
-        return True
 
 
 def main():
     start = time.time()
-
     string = generate_string()
 
-    while check_string(string):
-        string = generate_string()
-
     end = time.time()
-
+    
+    print(string)
     print("Time passed: " + str(end-start))
 
 main()
-    
-        
